2093c0dfeed4d4eb4ac8fc084863bca9d7e6f5fe
kutaydede <dedekutay34@gmail.com>
2022-09-16 15:00:35 +0300

    odev9 eklendi

---
+++ src/odev9/Main.java
@@ -0,0 +1,21 @@
+package odev9;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int i=1;
+        do {
+            System.out.println(i);
+            i+=2;
+
+        } while (i<10);
+        System.out.println("Do-While Döngüsü Bitti");
+
+
+        int j=20;
+        do {
+            System.out.println(j);
+        }while (j<10);
+        System.out.println("Do-While Döngüsü Bitti");
+    }
+}
