4d7d0434288ac1d1aebf5164293dac18f68ac03a
kutaydede <dedekutay34@gmail.com>
2022-09-16 14:40:16 +0300

    odev7 eklendi

---
+++ src/odev8/Main.java
@@ -0,0 +1,21 @@
+package odev8;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int i=2;
+        while (i<10){
+            System.out.println(i);
+            i++;
+        }
+
+        System.out.println("While Döngüsü Bitti");
+
+        int o=2;
+        while (o<10){
+            System.out.println(o);
+            o+=2;
+        }
+        System.out.println("While Döngüsü Bitti");
+    }
+}
